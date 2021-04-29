f71c8029c7d57ed49d8ae54914340175362a413a
Jake Deeks <49346100+DeeksJ@users.noreply.github.com>
2021-04-28 19:31:16 -0500

    Update registerUser.php

--- registerUser.php
+++ registerUser.php
@@ -7,7 +7,7 @@
 	$dbpassword = '';
 	$dbname= 'accounts';
 	
-	$conn = new mysqli("sql203.epizy.com", "epiz_28288046", "wSejTvlnICy", "epiz_28288046_fuelQuotes");
+	$conn = new mysqli("127.0.0.1", "root", "", "epiz_28288046_fuelQuotes");
 	
 	if(!$conn){
 		die("Connection Failed: ".mysqli_connect_error());
